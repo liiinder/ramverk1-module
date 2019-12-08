0df73ed2e1fd30e7b2e7bc894d28ad848262eadd
liiinder <linderkristoffer@hotmail.com>
2019-12-08 13:21:42 +0000

    revoked last patch

--- src/Model/Curl.php
+++ src/Model/Curl.php
@@ -25,18 +25,16 @@ class Curl
         ];
         //  Initiate curl handler
         $ch = curl_init();
-        if ($ch) {
-            // Set options
-            curl_setopt_array($ch, $options);
-            // Execute
-            $data = curl_exec($ch);
-            // Closing
-            curl_close($ch);
-            $res = json_decode($data, true);
-            
-            return $res;
-        }
-        return null;
+        // Set options
+        curl_setopt_array($ch, $options);
+        // Execute
+        $data = curl_exec($ch);
+        // Closing
+        curl_close($ch);
+        $res = json_decode($data, true);
+        
+        return $res;
+
     }
 
     /**
@@ -56,33 +54,30 @@ class Curl
         // Add all curl handlers and remember them
         // Initiate the multi curl handler
         $mh = curl_multi_init();
-        if ($mh) {
-            $chAll = [];
-            foreach ($urls as $url) {
-                $ch = curl_init($url);
-                curl_setopt_array($ch, $options);
-                curl_multi_add_handle($mh, $ch);
-                $chAll[] = $ch;
-            }
-            // Execute all queries simultaneously,
-            // and continue when all are complete
-            $running = null;
-            do {
-                curl_multi_exec($mh, $running);
-            } while ($running);
-            // Close the handles
-            foreach ($chAll as $ch) {
-                curl_multi_remove_handle($mh, $ch);
-            }
-            curl_multi_close($mh);
-            // All of our requests are done, we can now access the results
-            $response = [];
-            foreach ($chAll as $ch) {
-                $data = curl_multi_getcontent($ch);
-                $response[] = json_decode($data, true);
-            }
-            return $response;
+        $chAll = [];
+        foreach ($urls as $url) {
+            $ch = curl_init($url);
+            curl_setopt_array($ch, $options);
+            curl_multi_add_handle($mh, $ch);
+            $chAll[] = $ch;
         }
-        return null;
+        // Execute all queries simultaneously,
+        // and continue when all are complete
+        $running = null;
+        do {
+            curl_multi_exec($mh, $running);
+        } while ($running);
+        // Close the handles
+        foreach ($chAll as $ch) {
+            curl_multi_remove_handle($mh, $ch);
+        }
+        curl_multi_close($mh);
+        // All of our requests are done, we can now access the results
+        $response = [];
+        foreach ($chAll as $ch) {
+            $data = curl_multi_getcontent($ch);
+            $response[] = json_decode($data, true);
+        }
+        return $response;
     }
 }
